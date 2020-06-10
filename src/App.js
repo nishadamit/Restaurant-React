2cab6a65e7cb8f4d2bb4d701b19deebb492d771b
Amit Nishad <amitn590@gmail.com>
2020-06-11 01:44:18 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar,Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar bg="primary">
+       <Container>
+            <Navbar.Brand>BuzzTime</Navbar.Brand>
+       </Container>
+     </Navbar>
     </div>
   );
 }
